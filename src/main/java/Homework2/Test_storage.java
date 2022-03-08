78bb8cffaa7a17804ef98fb7122723d5ca4356df
Rurik <dragon_coffee@mail.ru>
2022-03-08 12:09:39 +0300

    1

--- src/main/java/Homework2/Test_storage.java
+++ src/main/java/Homework2/Test_storage.java
@@ -1,18 +1,24 @@
 package Homework2;
 
-import java.util.Collections;
-import java.util.Objects;
-import java.util.Scanner;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Test_storage {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //Для корректной работы, после команд используйте скобки (). Также не ставьте сразу 2 скобки(), сначала ставьте одну, какой-то товар, затем другую.
         //add (что-то) - добавление объекта на склад
         //remove (что-то) - удаление объектов со склада
         //contains (что-то) - проверка наличия объекта на складе
         //count () - количество объектов на складе
         //show () - сколько и каких объектов на складе
+        //export () - выгрузка добавленных объектов в Учётка.xls файл в папке проекта
         //exit () - выход с консоли
 
         FixedArrayList<String> storage = new FixedArrayList<>(18);
@@ -54,6 +60,21 @@ public class Test_storage {
             if (params[0].equals("show")){
                 System.out.println(storage.stream()
                         .collect(Collectors.groupingBy(Objects::toString, Collectors.counting() )));
+                continue;
+            };
+            int i = 0;
+
+            if (params[0].equals("export")) {
+                Workbook book = new HSSFWorkbook();
+                Sheet sheet = book.createSheet("Warehouse contents");
+                while (i < storage.size()) {
+                    Row row1 = sheet.createRow((short) i);
+                    row1.createCell((short) 0).setCellValue(storage.get(i));
+                    i++;
+            }
+                book.write(new FileOutputStream("Учётка.xls"));
+                book.close();
+
                 continue;
             }
             if (params[0].equals("exit")){
